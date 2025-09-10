300a186761e5bd684f2129c8b32a9a4fef1b2d2c
Raphael Chappert <raphael.chappert@hotmail.fr>
2025-09-10 09:49:13 +0200

    Refactor code structure for improved readability and maintainability

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+// @ts-check
+import createMDX from '@next/mdx';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkGfm from 'remark-gfm';
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
+};
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm, remarkFrontmatter],
+  },
+});
+
+export default withMDX(nextConfig);
