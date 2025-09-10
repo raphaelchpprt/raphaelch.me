03939827005024b051c2299654e5084635e86a8a
Raphael Chappert <raphael.chappert@hotmail.fr>
2025-09-10 18:01:37 +0200

    Refactor font imports and update layout component for consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
-import { Analytics } from '@vercel/analytics/react';
-import type { Metadata } from 'next';
-import { Inter, JetBrains_Mono } from 'next/font/google';
-import './globals.css';
+import { Analytics } from "@vercel/analytics/react";
+import type { Metadata } from "next";
+import { Golos_Text, JetBrains_Mono } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({
-  subsets: ['latin'],
-  weight: ['400', '600', '700'],
-  variable: '--font-inter',
+const sans = Golos_Text({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  variable: "--font-sans",
 });
 
-const jetbrains = JetBrains_Mono({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-jetbrains',
+const mono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-mono",
 });
 
 export const metadata: Metadata = {
-  title: 'raphaelch.me',
+  title: "raphaelch.me",
   description:
-    'Concepteur d’expériences digitales alliant design, ergonomie et accessibilité, j’accompagne les projets à impact positif dans la tech, la RSE et la transition énergétique.',
+    "Concepteur d’expériences digitales alliant design, ergonomie et accessibilité, j’accompagne les projets à impact positif dans la tech, la RSE et la transition énergétique.",
   icons:
     "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⛵️</text></svg>",
 };
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-black">
       <body
-        className={`${inter.variable} ${jetbrains.variable} relative antialiased`}
+        className={`${sans.variable} ${mono.variable} relative antialiased`}
       >
         <main className="bg-background relative z-10 min-h-screen pb-24">
           {children}
