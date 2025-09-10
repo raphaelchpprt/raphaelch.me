99da3883a5e32ab112dd9308a8ab35dfb32ac3c3
Raphael Chappert <raphael.chappert@hotmail.fr>
2025-09-10 18:01:33 +0200

    Add blog metadata and functions to retrieve blog posts

---
+++ lib/blog.ts
@@ -0,0 +1,39 @@
+export type Metadata = {
+  title: string;
+  publishedAt: string;
+  description: string;
+  slug: string;
+  canonicalUrl: string;
+  publish: boolean;
+};
+
+export async function getBlogPosts(): Promise<Metadata[]> {
+  const postsMetadata: Metadata[] = [];
+  for (const post of POSTS) {
+    const { metadata } = await import(`@/content/${post.slug}.mdx`);
+    postsMetadata.push(metadata);
+  }
+  return postsMetadata;
+}
+
+export async function getBlogPostMetadata(slug: string): Promise<Metadata> {
+  const { metadata } = await import(`@/content/${slug}.mdx`);
+  return metadata;
+}
+
+export const POSTS = [
+  {
+    title:
+      "Pourquoi l’expérience utilisateur est politique (et pas seulement esthétique)",
+    slug: "experience-utilisateur-politique",
+  },
+  {
+    title:
+      "Du carbone au code : ce que m’a appris la greentech sur le rôle du développeur",
+    slug: "carbone-code-role-developpeur",
+  },
+  {
+    title: "Le futur du front-end : interfaces plus humaines à l’ère de l’IA",
+    slug: "futur-front-end-interfaces-humaines-ia",
+  },
+];
