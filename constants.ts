8876ab355362ee2f24563a32bf5544b53936d454
Raphael Chappert <raphael.chappert@hotmail.fr>
2025-09-10 09:49:21 +0200

    Add constants for internal and external paths

---
+++ constants.ts
@@ -0,0 +1,11 @@
+export const paths = {
+  // internal
+  notes: (slug: string) => `/notes/${slug}`,
+
+  // external
+  github: 'https://github.com/raphaelchpprt',
+  //repo: "https://github.com/williamhzo/williamhzo.me",
+  linkedin: 'www.linkedin.com/in/raphaelchappert',
+  email: 'hi@raphaelch.me',
+  cv: 'https://cvws.icloud-content.com/B/AfEaSDCv2hFg6blkfp6pJ6ZicAPHARMx6TKkeuANzV_1Dh32syMbEpz6/CV_RaphaelCHAPPERT_Developpeur-web.pdf?o=AqZTp2AM1MbEFlOKXcSpTHRyB2L9AKAl4rMfZ6uT7QOd&v=1&x=3&a=CAogKYUpLE3eBvcBkGZbcXH9c0YMMF9dNLMacWBFZBpMneMSbRCU3rj7kjMYlLuU_ZIzIgEAUgRicAPHWgQbEpz6aiab1jmBDqt5dduvqvQiqGgUHnXvbK4pBiedSIBgJgW0-_PsyoadW3ImKSmkJr99UX_C_fww_ru-BF6Y_ltAfSeKedgAO5fkjd-YcoGYq-I&e=1757440974&fl=&r=715224b1-3682-4974-a0b7-f7fc3580e7d9-1&k=wHQ_qFY0VsrSl0ELtNe3bg&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=136&s=cA3inznZeMhDQiOTZU4WF_ajpOw&cd=i',
+};
