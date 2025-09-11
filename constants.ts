166766492cd0940b2c579c5685ef4240716639a9
Raphael Chappert <raphael.chappert@hotmail.fr>
2025-09-11 11:37:41 +0200

    Standardize string quotes and update CV link in constants

--- constants.ts
+++ constants.ts
@@ -3,9 +3,9 @@ export const paths = {
   notes: (slug: string) => `/notes/${slug}`,
 
   // external
-  github: 'https://github.com/raphaelchpprt',
+  github: "https://github.com/raphaelchpprt",
   //repo: "https://github.com/williamhzo/williamhzo.me",
-  linkedin: 'www.linkedin.com/in/raphaelchappert',
-  email: 'hi@raphaelch.me',
-  cv: 'https://cvws.icloud-content.com/B/AfEaSDCv2hFg6blkfp6pJ6ZicAPHARMx6TKkeuANzV_1Dh32syMbEpz6/CV_RaphaelCHAPPERT_Developpeur-web.pdf?o=AqZTp2AM1MbEFlOKXcSpTHRyB2L9AKAl4rMfZ6uT7QOd&v=1&x=3&a=CAogKYUpLE3eBvcBkGZbcXH9c0YMMF9dNLMacWBFZBpMneMSbRCU3rj7kjMYlLuU_ZIzIgEAUgRicAPHWgQbEpz6aiab1jmBDqt5dduvqvQiqGgUHnXvbK4pBiedSIBgJgW0-_PsyoadW3ImKSmkJr99UX_C_fww_ru-BF6Y_ltAfSeKedgAO5fkjd-YcoGYq-I&e=1757440974&fl=&r=715224b1-3682-4974-a0b7-f7fc3580e7d9-1&k=wHQ_qFY0VsrSl0ELtNe3bg&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=136&s=cA3inznZeMhDQiOTZU4WF_ajpOw&cd=i',
+  linkedin: "www.linkedin.com/in/raphaelchappert",
+  email: "hi@raphaelch.me",
+  cv: "https://cvws.icloud-content.com/B/AcuehNhFJiJBzklkWEPRLASTzea9AcmMbctwGVNp5wVZXLfYFB2XXJzC/CV_RaphaelCHAPPERT_Developpeur-software-ingenieur-full-stack-front-end.pdf?o=AvGATJXHMQHJK8L6eDCn79Yer74C0fJrXbVvjqlFxd2-&v=1&x=3&a=CAogb4YubinMhxBYMepvPc7Jh8x0q3QMqKLDBwu-hdYp2MESbRC3jNrAkzMYt-m1wpMzIgEAUgSTzea9WgSXXJzCaiagpTF5QyucAurwMMufIq84kWi4aSuCaVjax56nlXEJfuAylYkN33ImQ-dq5JBsQSd4m4grxR9hlwH21Q3N1ejsoZSfebBeI07Dioh8hvg&e=1757586224&fl=&r=8deb21d3-5914-48e1-8d7d-9a9c51006433-1&k=ob8vuRdrRDXaLaY3b_FYnA&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=136&s=8FrO-D9Ryv37J9mduDh1W_oYEWo&cd=i",
 };
