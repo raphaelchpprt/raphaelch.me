ba2888753de84d40ec4d8ed037e38e71e53c5deb
Raphael Chappert <raphael.chappert@hotmail.fr>
2025-09-15 12:30:42 +0200

    Update repository link in constants and ensure email and CV links are correctly formatted

--- constants.ts
+++ constants.ts
@@ -4,8 +4,9 @@ export const paths = {
 
   // external
   github: "https://github.com/raphaelchpprt",
-  //repo: "https://github.com/williamhzo/williamhzo.me",
+  repo: "https://github.com/raphaelchpprt/raphaelch.me",
   linkedin: "www.linkedin.com/in/raphaelchappert",
-  email: "hi@raphaelch.me",
-  cv: "https://cvws.icloud-content.com/B/AcuehNhFJiJBzklkWEPRLASTzea9AcmMbctwGVNp5wVZXLfYFB2XXJzC/CV_RaphaelCHAPPERT_Developpeur-software-ingenieur-full-stack-front-end.pdf?o=AvGATJXHMQHJK8L6eDCn79Yer74C0fJrXbVvjqlFxd2-&v=1&x=3&a=CAogb4YubinMhxBYMepvPc7Jh8x0q3QMqKLDBwu-hdYp2MESbRC3jNrAkzMYt-m1wpMzIgEAUgSTzea9WgSXXJzCaiagpTF5QyucAurwMMufIq84kWi4aSuCaVjax56nlXEJfuAylYkN33ImQ-dq5JBsQSd4m4grxR9hlwH21Q3N1ejsoZSfebBeI07Dioh8hvg&e=1757586224&fl=&r=8deb21d3-5914-48e1-8d7d-9a9c51006433-1&k=ob8vuRdrRDXaLaY3b_FYnA&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=136&s=8FrO-D9Ryv37J9mduDh1W_oYEWo&cd=i",
+  malt: "https://www.malt.fr/profile/raphaelchappert",
+  email: "mailto:hi@raphaelch.me",
+  cv: "https://cvws.icloud-content.com/B/AcuehNhFJiJBzklkWEPRLASTzea9AcmMbctwGVNp5wVZXLfYFB2XXJzC/CV_RaphaelCHAPPERT_Developpeur-software-ingenieur-full-stack-front-end.pdf?o=AkW6EeS450qvsv6Fz6G8w-8C9d7-31QV-NaKEdKTcNyc&v=1&x=3&a=CAogSgbzdUQwniilzzeU2K6cCLDxXlTwIoS78wYF0Ds-Hp4SbRDTq6HMlDMY04j9zZQzIgEAUgSTzea9WgSXXJzCaiZWOrYJTnobr-2M8MK4Yy8uK9ko9YXgbnGKFMRG-Xy1cmxkQn6kqHImhLdrWUk8hstyXrV2n8JN7HWv2zPt3SdTuq6cvqmfLinxGLTuCNw&e=1757878895&fl=&r=97889070-3b6c-4bf0-9f03-89bc40f66299-1&k=ob8vuRdrRDXaLaY3b_FYnA&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=136&s=7MWYpUKLlO5MMu4H5DmFYAZinkY&cd=i",
 };
