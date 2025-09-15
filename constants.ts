7a9eb77a51fab0236a3de073f16fd1af7265c6f4
Raphael Chappert <raphael.chappert@hotmail.fr>
2025-09-15 16:22:48 +0200

    Update CV link to use a relative path for consistency

--- constants.ts
+++ constants.ts
@@ -8,5 +8,5 @@ export const paths = {
   linkedin: "www.linkedin.com/in/raphaelchappert",
   malt: "https://www.malt.fr/profile/raphaelchappert",
   email: "mailto:hi@raphaelch.me",
-  cv: "https://cvws.icloud-content.com/B/AcuehNhFJiJBzklkWEPRLASTzea9AcmMbctwGVNp5wVZXLfYFB2XXJzC/CV_RaphaelCHAPPERT_Developpeur-software-ingenieur-full-stack-front-end.pdf?o=AkW6EeS450qvsv6Fz6G8w-8C9d7-31QV-NaKEdKTcNyc&v=1&x=3&a=CAogSgbzdUQwniilzzeU2K6cCLDxXlTwIoS78wYF0Ds-Hp4SbRDTq6HMlDMY04j9zZQzIgEAUgSTzea9WgSXXJzCaiZWOrYJTnobr-2M8MK4Yy8uK9ko9YXgbnGKFMRG-Xy1cmxkQn6kqHImhLdrWUk8hstyXrV2n8JN7HWv2zPt3SdTuq6cvqmfLinxGLTuCNw&e=1757878895&fl=&r=97889070-3b6c-4bf0-9f03-89bc40f66299-1&k=ob8vuRdrRDXaLaY3b_FYnA&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=136&s=7MWYpUKLlO5MMu4H5DmFYAZinkY&cd=i",
+  cv: "/cv-raphael-chappert-software-developer.pdf",
 };
