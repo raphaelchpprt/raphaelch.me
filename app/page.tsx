a76351f6128fd34f933fd4a7b88220725b398ea8
Raphael Chappert <raphael.chappert@hotmail.fr>
2025-09-15 20:26:39 +0200

    Add SpinningText component for animated text effects and integrate it into the Home component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { InlineLink } from "@/components/link";
 import { paths } from "@/constants";
 import { Hero } from "@/components/hero";
 import { getBlogPosts } from "@/lib/blog";
+import { SpinningText } from "@/components/spinning-text";
 import {
   ReactIcon,
   NextIcon,
@@ -21,8 +22,17 @@ export default async function Home() {
   return (
     <div className="mx-auto flex max-w-screen-md flex-col gap-12 px-6 pt-24 font-mono">
       <div className="mx-auto flex w-full max-w-screen-xl flex-col gap-4">
-        <div className="group -mt-8 flex items-center justify-end gap-32"></div>
-        <Hero />
+        <div className="group -mt-8 flex items-center justify-end gap-32">
+          <SpinningText
+            className="-mt-4 font-mono text-sm uppercase opacity-0 transition-opacity duration-750 group-hover:opacity-100"
+            radius={5.5}
+            duration={15}
+          >
+            {`craft • interface • design • greentech • `}
+          </SpinningText>
+          <Hero />
+        </div>
+
         <div className="group mt-12 flex max-w-prose flex-col gap-6">
           <h2 className="text-balance">
             un <strong>développeur full-stack</strong> avec un intérêt marqué
@@ -69,67 +79,67 @@ export default async function Home() {
         <div className="flex flex-col gap-6">
           <p className="max-w-prose text-pretty">
             ces 4 dernières années, j’ai eu la chance de contribuer au
-            développement d&rsquo;une plateforme SaaS portée par une startup qui
-            réalise les bilans et stratégies carbone d&rsquo;entreprises
-            privées, d&rsquo;ONG et d&rsquo;agences des Nations Unies et de
-            l&rsquo;UE
+            développement d&rsquo;une <strong>plateforme saas</strong> portée
+            par une startup qui réalise les bilans et stratégies carbone
+            d&rsquo;entreprises privées, d&rsquo;ONG, d&rsquo;agences des
+            nations unies et de l&rsquo;union européenne
           </p>
           <p className="max-w-prose text-pretty">
-            en tant qu&rsquo;ingénieur produit, j&apos;ai participé à la
-            création de fonctionnalités front et back-end, mettant l&apos;accent
-            sur l&apos;expérience utilisateur afin d&rsquo;optimiser une gestion
-            applicative de données complexes à forte volumétrie
+            en tant qu&rsquo;ingénieur produit au sein de l&rsquo;équipe tech,
+            j&apos;ai participé à la création de fonctionnalités front et
+            back-end, mettant l&apos;accent sur l&apos;
+            <strong>expérience utilisateur</strong> afin d&rsquo;optimiser une
+            gestion applicative de données complexes à forte volumétrie
           </p>
           <p className="max-w-prose text-pretty">
-            je mets aujourd&apos;hui ces compétences à profit pour contribuer à
-            la conception de produits visant à apporter des réponses tech aux
-            problèmes que peut traverser la société avec comme objectif : être
-            simples à la prise en main et naturel à l&rsquo;utilisation
+            je me consacre aujourd’hui à la conception de produits visant à{" "}
+            <strong>apporter des réponses</strong> tech aux problèmes sociaux,
+            écologiques ou numériques que peut traverser la société, avec comme
+            objectif : être simples à la prise en main et naturels à
+            l&rsquo;utilisation
           </p>
         </div>
       </div>
       <div>
         <h3 className="text-accent mb-4 font-sans font-semibold">stacks</h3>
         <div className="flex flex-col gap-6">
-          <div className="flex max-w-prose items-center text-pretty">
-            <div className="mr-4 flex items-center space-x-2">
+          <p className="flex max-w-prose flex-wrap leading-loose text-pretty">
+            <span className="mr-4 flex items-center space-x-2">
               <StackIcon icon={ReactIcon} />
               <span>react</span>
-            </div>
-            <div className="mr-4 flex items-center space-x-2">
+            </span>
+            <span className="mr-4 flex items-center space-x-2">
               <StackIcon icon={NextIcon} /> <span>next</span>
-            </div>
-            <div className="mr-4 flex items-center space-x-2">
+            </span>
+            <span className="mr-4 flex items-center space-x-2">
               <StackIcon icon={TypescriptIcon} />
               <span>typescript</span>
-            </div>
-            <div className="mr-4 flex items-center space-x-2">
+            </span>
+            <span className="mr-4 flex items-center space-x-2">
               <StackIcon icon={TailwindIcon} />
               <span>tailwind</span>
-            </div>
-            <div className="mr-4 flex items-center space-x-2">
+            </span>
+            <span className="mr-4 flex items-center space-x-2">
               <StackIcon icon={MaterialUiIcon} />
-              <span>materialUi</span>
-            </div>
-          </div>
-          <div className="flex max-w-prose items-center space-x-4 text-pretty">
-            <div className="flex items-center space-x-2">
+              <span>materialui</span>
+            </span>
+            <span className="mr-4 flex items-center space-x-2">
               <StackIcon icon={NodeIcon} />
               <span>node</span>
-            </div>
-            <div className="flex items-center space-x-2">
+            </span>
+            <span className="mr-4 flex items-center space-x-2">
               <StackIcon icon={MongodbIcon} />
               <span>mongodb</span>
-            </div>
-            <div className="flex items-center space-x-2">
+            </span>
+            <span className="mr-4 flex items-center space-x-2">
               <StackIcon icon={PostgresqlIcon} />
               <span>postgresql</span>
-            </div>
-            <div className="flex items-center space-x-2">
+            </span>
+            <span className="mr-4 flex items-center space-x-2">
               <StackIcon icon={FigmaIcon} />
               <span>figma</span>
-            </div>
-          </div>
+            </span>
+          </p>
         </div>
       </div>
       <div>
@@ -137,9 +147,9 @@ export default async function Home() {
         <div className="flex flex-col gap-6">
           <p className="max-w-prose text-pretty">
             tu peux me contacter par{" "}
-            <InlineLink href={paths.email}>email</InlineLink>, mon cv complet
-            s&rsquo;affiche <InlineLink href={paths.cv}>ici</InlineLink> et tu
-            peux consulter mes projets sur{" "}
+            <InlineLink href={paths.email}>mail</InlineLink>, mon cv complet
+            s&rsquo;affiche <InlineLink href={paths.cv}>ici</InlineLink>, tu
+            peux aussi consulter mes projets sur{" "}
             <InlineLink href={paths.github}>github</InlineLink>
           </p>
 
@@ -158,5 +168,5 @@ export default async function Home() {
 }
 
 const StackIcon = ({ icon: Icon }: { icon: ElementType }) => {
-  return <Icon className="text-muted-foreground size-5" />;
+  return <Icon className="text-muted-foreground size-4" />;
 };
