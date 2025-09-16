e9fe4baff4e5928704d263a5c9a885c827cecebc
Raphael Chappert <raphael.chappert@hotmail.fr>
2025-09-16 17:04:52 +0200

    Add layout and page components for dynamic blog post rendering

---
+++ app/notes/[slug]/layout.tsx
@@ -0,0 +1,31 @@
+import { getBlogPostMetadata } from "@/lib/blog";
+import { Metadata } from "next";
+import Link from "next/link";
+
+type Props = {
+  params: Promise<{ slug: string }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const slug = (await params).slug;
+  const post = await getBlogPostMetadata(slug);
+
+  return {
+    title: `${post.title} — raphael`,
+    description: post.description,
+  };
+}
+
+export default function MdxLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <section className="prose mx-auto flex flex-col items-start gap-6 p-5 pt-24 font-sans">
+      <Link
+        href="/"
+        className="text-muted-foreground hover:text-accent font-mono font-normal no-underline transition-colors duration-150"
+      >
+        home
+      </Link>
+      {children}
+    </section>
+  );
+}
---
+++ app/notes/[slug]/page.tsx
@@ -0,0 +1,36 @@
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const slug = (await params).slug;
+  const { default: Post, metadata } = await import(`@/content/${slug}.mdx`);
+
+  const dateObj = new Date(metadata.publishedDate);
+  const formattedDate = dateObj.toLocaleDateString("us-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <div>
+      <div className="mb-12 flex flex-col">
+        <h1>{metadata.title}</h1>
+        <p>{metadata.description}</p>
+        <small className="italic">{formattedDate}</small>
+      </div>
+      <Post />
+    </div>
+  );
+}
+
+export function generateStaticParams() {
+  return [
+    { slug: "experience-utilisateur-politique" },
+    { slug: "carbone-code-role-developpeur" },
+    { slug: "futur-front-end-interfaces-humaines-ia" },
+  ];
+}
+
+export const dynamicParams = false;
